dff355f862c6e9ce40a03c85d992be9af625ae11
A CHARAN REDDY <31070781+acharanreddy@users.noreply.github.com>
2017-08-26 01:26:35 -0400

    Update resumeparsing.py

--- resumeparsing.py
+++ resumeparsing.py
@@ -4,25 +4,25 @@ import re
 class ResumeParsing:
     def parseEmail():
         tika.initVM()
-        parsed = parser.from_file("MyResume2.docx")
+        parsed = parser.from_file("MyResume.docx")
         regular_expression = re.compile(r"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}", re.IGNORECASE)
         result = re.search(regular_expression,parsed["content"])
         print("Email id:"+result.group())
     def parsePhone():
         tika.initVM()
-        parsed = parser.from_file("MyResume2.docx")
-        regular_expression = re.compile(r"\d{3}-\d{3}-\d{4}")
+        parsed = parser.from_file("MyResume.docx")
+        regular_expression = re.compile(r"[[+91]*[' ']*[0-9]{10}]*",re.IGNORECASE)
         result = re.search(regular_expression,parsed["content"])
         print("phone No:"+result.group())
     def parseAddress():
         tika.initVM()
-        parsed = parser.from_file("MyResume2.docx")
+        parsed = parser.from_file("MyResume.docx")
         regular_expression = re.compile(r"[0-9]{1,4}\ [\w\s]+\, [\w]+\ [0-9]{1,5}", re.IGNORECASE)
         result = re.search(regular_expression,parsed["content"])
         print("Address:"+result.group())
     def parseName():
         tika.initVM()
-        parsed = parser.from_file("MyResume2.docx")
+        parsed = parser.from_file("MyResume.docx")
         regular_expression = re.compile(r"[\w]+\s[\w]+", re.IGNORECASE)
         result = re.search(regular_expression,parsed["content"])
         print("Name:"+result.group())
